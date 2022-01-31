99ef4ea1bcccd403bfbb61864635b19655f2fef4
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-01-31 22:35:36 +0530

    added app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container } from "@material-ui/core";
+import PostDetails from "./components/PostDetails/PostDetails";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Redirect,
+  Navigate,
+} from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+// import { Navbar } from "./components/Navbar/Navbar";
+// import { Home } from "./components/Home/Home";
+import { Auth } from "./components/Auth/Auth";
 
-function App() {
+const App = () => {
+  const user = JSON.parse(localStorage.getItem("profile"));
+  // console.log(user, "lol");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Container maxwidth="xl">
+        <Routes>
+          <Route path="/" exact element={<Auth />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
-}
-
+};
 export default App;
