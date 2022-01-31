ff258371e3bdfd8f5147e65f74cf69b5d01efa0e
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-01-31 23:04:22 +0530

    added auth styles inputs

--- src/components/Auth/styles.js
+++ src/components/Auth/styles.js
@@ -1,43 +1,30 @@
-import React from "react";
-import { TextField, Grid, InputAdornment, IconButton } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-import Visibility from "@material-ui/icons/Visibility";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
-
-const Input = ({
-  name,
-  handleChange,
-  label,
-  half,
-  autoFocus,
-  type,
-  handleShowPassword,
-}) => (
-  <Grid item xs={12} sm={half ? 6 : 12}>
-    <TextField
-      name={name}
-      onChange={handleChange}
-      variant="outlined"
-      required
-      fullWidth
-      label={label}
-      autoFocus={autoFocus}
-      type={type}
-      InputProps={
-        name === "password"
-          ? {
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={handleShowPassword}>
-                    {type === "password" ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }
-          : null
-      }
-    />
-  </Grid>
-);
-
-export default Input;
+export default makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: theme.spacing(2),
+  },
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+    },
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  googleButton: {
+    marginBottom: theme.spacing(2),
+  },
+}));
