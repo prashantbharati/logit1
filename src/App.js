664eed0f379dd88ebe203a99fcf2fcdecfc1764a
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-01-31 22:49:23 +0530

    added auth page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import PostDetails from "./components/PostDetails/PostDetails";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Redirect,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import { useNavigate } from "react-router-dom";
 // import { Navbar } from "./components/Navbar/Navbar";
 // import { Home } from "./components/Home/Home";
 import { Auth } from "./components/Auth/Auth";
 
 const App = () => {
-  const user = JSON.parse(localStorage.getItem("profile"));
+  // const user = JSON.parse(localStorage.getItem("profile"));
   // console.log(user, "lol");
   return (
     <BrowserRouter>
